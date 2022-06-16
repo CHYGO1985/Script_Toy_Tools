7fd3a5616742b5c79ba406dbde34614abf2c6680
CHYGO1985 <jingjiejiang85@gmail.com>
2022-06-16 12:27:08 +0800

    revise download cleaner

--- File_cleaner_mac/download_cleaner.sh
+++ File_cleaner_mac/download_cleaner.sh
@@ -1,2 +1,6 @@
 # clean torrent files
 find . -type f -name '*.torrent' -delete
+find . -type f -name '*.png' -delete
+find . -type f -name '*.jpg' -delete
+find . -type f -name '*.zip' -delete
+find . -type f -name '*.dmg' -delete
